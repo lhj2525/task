db5bdf86625d939379fb73638b0a7b0b5d72785d
lhj2525 <39327533+lhj2525@users.noreply.github.com>
2021-04-02 10:14:07 +0800

    Update 潜鸟/DIY_shopsign.js

--- 潜鸟/DIY_shopsign.js
+++ 潜鸟/DIY_shopsign.js
@@ -1,14 +1,13 @@
 /*
-店铺签到，各类店铺签到，有新的店铺直接添加token即可
 ============Quantumultx===============
 [task_local]
 #店铺签到
-0 1 * * * https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.js, tag=店铺签到, enabled=true
+0 1 * * * https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.jss, tag=店铺签到, enabled=true
 ===========Loon============
 [Script]
-cron "0 1 * * *" script-path=https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.js,tag=店铺签到
+cron "0 1 * * *" script-path=https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.jss,tag=店铺签到
 ============Surge=============
-店铺签到 = type=cron,cronexp="0 1 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.js
+店铺签到 = type=cron,cronexp="0 1 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.jss
 ===========小火箭========
 店铺签到 = type=cron,script-path=https://raw.githubusercontent.com/Misaka09982/AutoSignMachine/master/DIY_shopsign.jss, cronexpr="0 1 * * *", timeout=3600, enable=true
 */
